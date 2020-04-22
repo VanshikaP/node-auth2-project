faefdeeb9da9b1fdfb7d6eaa1704ed59d0ac2c4e
VanshikaP <vanshikapundir92@gmail.com>
2020-04-22 10:30:31 -0700

    knex and endpoints

---
+++ users/users-model.js
@@ -0,0 +1,34 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  findByDepartment
+};
+
+function find() {
+  return db("users").select("id", "username");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function add(user) {
+  const [id] = await db("users").insert(user, "id");
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db("users")
+    .select('id', 'username', 'password')
+    .where({ id })
+    .first();
+}
+
+function findByDepartment(department) {
+    return db('users').where({department});
+}
